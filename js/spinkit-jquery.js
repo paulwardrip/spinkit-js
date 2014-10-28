16c12cb6811bfaaacf72b24275025e158ab8679c
Paul Wardrip <pwardrip@gmail.com>
2014-10-28 13:49:17 -0400

    Fix to make angular and jquery use the same attribute "type" to specify which spinner to show.

--- js/spinkit-jquery.js
+++ js/spinkit-jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("spinner").each(
 		function(){
 			var target = $(this);
-			var spinclass = target.attr("class");
+			var spinclass = target.attr("type");
 			if (!spinclass) spinclass = "square";
 			var html = "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/spinkit-" + spinclass + ".css\" />" +
 					"<div class=\"spinner\">" +
