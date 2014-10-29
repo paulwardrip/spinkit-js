4f462a40294a41099ee7d9bf9959407d88678007
Paul Wardrip <pwardrip@gmail.com>
2014-10-29 13:19:00 -0400

    Examples of all spinners.

--- js/spinkit-jquery.js
+++ js/spinkit-jquery.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 	$("spinner").each(
 		function(){
 			var target = $(this);
+			console.log(target);
 			var spinclass = target.attr("type");
 			if (!spinclass) spinclass = "square";
-			var html = "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/spinkit-" + spinclass + ".css\" />" +
-					"<div class=\"spinner\">" +
-						spinkit.render(spinclass);
+			var html = "<div class=\"spinner\">" +
+					"<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/spinkit-" + spinclass + ".css\" />" +
+					spinkit.render(spinclass);
 					"</div>";
 			$(html).insertBefore(target);
 			target.detach();
